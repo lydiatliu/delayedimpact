5a68a8947bbc6edfb8944195eea80fd6a03eb4b7
lydiatliu <lydiatliu@berkeley.edu>
2018-09-14 18:33:03 -0700

    cleaning

--- distribution_to_loans_outcomes.py
+++ distribution_to_loans_outcomes.py
@@ -1,132 +1,156 @@
+"""Compute and Visualize outcome curves"""
+
 import numpy as np
-import matplotlib.pyplot as plt
-import math
 import solve_credit as sc
-import prob_util_functions as puf
-from scipy.stats import binom 
 
 
-
-def get_thresholds(loan_repaid_probs,pis,group_size_ratio,utils,score_change_fns,scores):
+def get_thresholds(loan_repaid_probs, pis, group_size_ratio, utils, score_change_fns, scores):
+    """get thresholds for all 3 fairness criteria"""
     # unpacking bank utilities
     utility_default, utility_repaid = utils
-    break_even_prob = utility_default/(utility_default-utility_repaid)
+    break_even_prob = utility_default / (utility_default - utility_repaid)
 
     # getting loan policies
-    data = sc.fairness_data(get_rhos(loan_repaid_probs,scores), pis,
-                                                group_size_ratio, break_even_prob)
-    tau_MP = data.max_profit_loans()
-    tau_DP = data.demographic_loans()
-    tau_EO = data.equal_opp_loans()
+    data = sc.fairness_data(get_rhos(loan_repaid_probs, scores), pis,
+                            group_size_ratio, break_even_prob)
+    tau_maxprof = data.max_profit_loans()
+    tau_dempar = data.demographic_loans()
+    tau_eqopp = data.equal_opp_loans()
 
-    thresh_MP = get_thresholds_from_taus(tau_MP,scores)
-    thresh_DP = get_thresholds_from_taus(tau_DP,scores)
-    thresh_EO = get_thresholds_from_taus(tau_EO,scores)
+    thresh_maxprof = get_thresholds_from_taus(tau_maxprof, scores)
+    thresh_dempar = get_thresholds_from_taus(tau_dempar, scores)
+    thresh_eqopp = get_thresholds_from_taus(tau_eqopp, scores)
 
     # unpacking repay probability as a function of score
-    loan_repay_fns = [lambda x: loan_repaid_prob(x) for loan_repaid_prob in loan_repaid_probs]
+    loan_repay_fns = [lambda x: loan_repaid_prob(x) for
+                      loan_repaid_prob in loan_repaid_probs]
     # getting threshold at which average score change becomes negative
-    thresh_downwards = [get_mean_movement_threshold(pis[0], scores, loan_repay_fn, score_change_fns) for loan_repay_fn in loan_repay_fns]
+    thresh_downwards = [get_mean_movement_threshold(pis[0], scores, loan_repay_fn,
+                                                    score_change_fns) for loan_repay_fn
+                        in loan_repay_fns]
 
-    return thresh_DP, thresh_EO, thresh_MP, thresh_downwards
+    return thresh_dempar, thresh_eqopp, thresh_maxprof, thresh_downwards
 
-def get_outcome_curve(loan_repay_fn,pi,scores,impacts):
+
+def get_outcome_curve(loan_repay_fn, pi, scores, impacts):
+    """compute the outcome curve"""
     delta_mu = np.zeros(scores.size)
-    delta_mu[0] = exp_move(scores[-1], loan_repay_fn,bounds=[300,850], move_vec=impacts)*pi[-1]
-    for i in range(1,scores.size):
-        delta_mu[i] = exp_move(scores[-(i+1)], loan_repay_fn,bounds=[300,850], move_vec=impacts)*pi[-(i+1)] + delta_mu[i-1]
+    delta_mu[0] = exp_move(scores[-1], loan_repay_fn, bounds=[300, 850],
+                           move_vec=impacts) * pi[-1]
+    for i in range(1, scores.size):
+        delta_mu[i] = exp_move(scores[-(i + 1)], loan_repay_fn, bounds=[300, 850],
+                               move_vec=impacts) * pi[-(i + 1)] + delta_mu[i - 1]
 
     return delta_mu
 
-def get_utility_curve(loan_repay_fns,pis,scores,utils):
-    Util = np.zeros([2,scores.size])
+
+def get_utility_curve(loan_repay_fns, pis, scores, utils):
+    """compute the institution's utility curve"""
+    util = np.zeros([2, scores.size])
     for j in range(2):
-        Util[j,0] = bank_util(scores[-1], loan_repay_fns[j], utils= utils)*pis[j,-1]
-        for i in range(1,scores.size):
-            Util[j,i] =  bank_util(scores[-(i+1)], loan_repay_fns[j], utils= utils)*pis[j,-(i+1)] + Util[j,i-1]
-    return Util
+        util[j, 0] = bank_util(scores[-1], loan_repay_fns[j], utils=utils) * pis[j, -1]
+        for i in range(1, scores.size):
+            util[j, i] = bank_util(scores[-(i + 1)], loan_repay_fns[j], utils=utils) * \
+                         pis[j, -(i + 1)] + util[j, i - 1]
+    return util
 
-def get_utility_curves_DP(Util,cdfs,group_size_ratio,scores):
-    Util_total = np.zeros([2,scores.size])
-    cdfs[0] = list(reversed(1-cdfs[0]))
-    cdfs[1] = list(reversed(1-cdfs[1]))
 
-    for i_A in range(scores.size):
-        prop_A = cdfs[0,i_A]
-        i_B = find_nearest(cdfs[1],prop_A)
-        Util_total[0, i_A] = group_size_ratio[0]*Util[0,i_A] + group_size_ratio[1]*Util[1,i_B]
+def get_utility_curves_dempar(util, cdfs, group_size_ratio, scores):
+    """compute the institution's utility curve under demographic parity constraint"""
+    util_total = np.zeros([2, scores.size])
+    cdfs[0] = list(reversed(1 - cdfs[0]))
+    cdfs[1] = list(reversed(1 - cdfs[1]))
 
-    for i_B in range(scores.size):
-        prop_B = cdfs[1,i_B]
-        i_A = find_nearest(cdfs[0],prop_B)
-        Util_total[1, i_B] = group_size_ratio[0]*Util[0,i_A] + group_size_ratio[1]*Util[1,i_B]
+    for i_a in range(scores.size):
+        prop_a = cdfs[0, i_a]
+        i_b = find_nearest(cdfs[1], prop_a)
+        util_total[0, i_a] = group_size_ratio[0] * util[0, i_a] + group_size_ratio[1] * util[1, i_b]
 
-    return Util_total
+    for i_b in range(scores.size):
+        prop_b = cdfs[1, i_b]
+        i_a = find_nearest(cdfs[0], prop_b)
+        util_total[1, i_b] = group_size_ratio[0] * util[0, i_a] + group_size_ratio[1] * util[1, i_b]
 
-def get_utility_curves_EO(Util,loan_repaid_probs,pis,group_size_ratio,scores):
+    return util_total
+
+
+def get_utility_curves_eqopp(util, loan_repaid_probs, pis, group_size_ratio, scores):
+    """compute the institution's utility curve under equal opportunity constraint"""
     rescaled_pis = np.zeros(pis.shape)
-    N_groups, N_scores = pis.shape
-    for group in range(N_groups):
-        for x in range(N_scores):
-            rescaled_pis[group,x] = pis[group,x] * loan_repaid_probs[group](scores[x])
+    n_groups, n_scores = pis.shape
+    for group in range(n_groups):
+        for x in range(n_scores):
+            rescaled_pis[group, x] = pis[group, x] * loan_repaid_probs[group](scores[x])
         rescaled_pis[group] = rescaled_pis[group] / np.sum(rescaled_pis[group])
 
     cdfs = np.zeros(pis.shape)
-    for group in range(N_groups):
-        cdfs[group,0] = rescaled_pis[group,0]
-        for x in range(1,N_scores):
-            cdfs[group,x] = rescaled_pis[group,x] + cdfs[group,x-1]
-    return get_utility_curves_DP(Util,cdfs,group_size_ratio, scores)
+    for group in range(n_groups):
+        cdfs[group, 0] = rescaled_pis[group, 0]
+        for x in range(1, n_scores):
+            cdfs[group, x] = rescaled_pis[group, x] + cdfs[group, x - 1]
+    return get_utility_curves_dempar(util, cdfs, group_size_ratio, scores)
 
-########## helper functions #####################################################
 
-def get_rhos(loan_repaid_probs,scores):
+def get_rhos(loan_repaid_probs, scores):
     """set up rhos[i,j] = probability of group i member repaying loan at state j"""
-    N_scores = len(scores)
-    N_groups = len(loan_repaid_probs)
-    rhos = np.zeros((N_groups,N_scores))
-    for j,s in enumerate(scores):
-        for i in range(N_groups):
-            rhos[i,j] = loan_repaid_probs[i](s)
+    n_scores = len(scores)
+    n_groups = len(loan_repaid_probs)
+    rhos = np.zeros((n_groups, n_scores))
+    for j, s in enumerate(scores):
+        for i in range(n_groups):
+            rhos[i, j] = loan_repaid_probs[i](s)
     return rhos
 
-# randomized threshold below which group's mean score will decrease, above which it increases
+
 def get_mean_movement_threshold(pi, scores, loan_repay_fn, score_change_fns, compare_pt=0.0):
+    """randomized threshold below which group's mean score will decrease,
+    above which it increases"""
     running_sum = 0
-    for i,s in enumerate(reversed(scores)):
-        x = len(scores) - 1 - i 
-        if ((running_sum + pi[x] * exp_move(s, loan_repay_fn, move_vec=score_change_fns)) < compare_pt):
-            randomized = (compare_pt-running_sum) /(exp_move(s, loan_repay_fn, move_vec=score_change_fns)*pi[x])
+    for i, s in enumerate(reversed(scores)):
+        x = len(scores) - 1 - i
+        if ((running_sum + pi[x] * exp_move(s, loan_repay_fn, move_vec=score_change_fns))
+                < compare_pt):
+            randomized = (compare_pt - running_sum) / (exp_move(s, loan_repay_fn,
+                                                                move_vec=score_change_fns) * pi[x])
             assert randomized >= 0
             assert randomized <= 1
             if i == 0:
                 return s
-            return s + randomized * abs(scores[x] - scores[x+1])
+            return s + randomized * abs(scores[x] - scores[x + 1])
         else:
             running_sum += pi[x] * exp_move(s, loan_repay_fn)
     return s
 
-def exp_move(x, loan_repay_fn, bounds=[300,850], move_vec= [-150,75]):
-    move_down = move_vec[0] 
-    move_up = move_vec[1] 
-    move = (1-loan_repay_fn(x))*move_down + loan_repay_fn(x)*move_up
-    move_to_within_bounds = max(min(x+move, bounds[1]),bounds[0])
+
+def exp_move(x, loan_repay_fn, bounds=[300, 850], move_vec=[-150, 75]):
+    """compute expected move in score"""
+    move_down = move_vec[0]
+    move_up = move_vec[1]
+    move = (1 - loan_repay_fn(x)) * move_down + loan_repay_fn(x) * move_up
+    move_to_within_bounds = max(min(x + move, bounds[1]), bounds[0])
     move_within_bounds = move_to_within_bounds - x
     return move_within_bounds
 
-def bank_util(x, loan_repay_fn, utils):
-    util_repay = utils[1] 
-    util_def = utils[0] 
-    return (1-loan_repay_fn(x))*util_def + loan_repay_fn(x)*util_repay
 
-# helper functions for turning loaning policies into thresholds for visualization
-def get_thresholds_from_taus(taus,scores):
+def bank_util(x, loan_repay_fn, utils):
+    """bank's expected utility"""
+    util_repay = utils[1]
+    util_def = utils[0]
+    return (1 - loan_repay_fn(x)) * util_def + loan_repay_fn(x) * util_repay
+
+
+def get_thresholds_from_taus(taus, scores):
+    """Turn loaning policies into thresholds for visualization"""
     thresholds = []
     for tau in taus:
-        x = np.amin(np.where(tau>0))
-        thresholds.append(scores[x] + (1-tau[x])*np.abs(scores[x+1]-scores[x])) #to visualize the randomization
+        x = np.amin(np.where(tau > 0))
+        # to visualize the randomization
+        thresholds.append(
+            scores[x] + (1 - tau[x]) * np.abs(scores[x + 1] - scores[x]))
     return thresholds
 
-def find_nearest(array,value):
-    idx = (np.abs(array-value)).argmin()
+
+def find_nearest(array, value):
+    """find closest value"""
+    idx = (np.abs(array - value)).argmin()
     return int(np.amin(idx))
--- solve_credit.py
+++ solve_credit.py
@@ -1,87 +1,101 @@
+"""Solve for loan thresholds under fairness criteria"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-# binary search implementation
+
 def binary_search(f, target, left=1e-5, right=1 - 1e-5, tol=1e-8):
+    """Binary search implementation"""
     midpoint = (left + right) * .5
     if abs(midpoint - left) < tol:
         return midpoint
     elif f(midpoint) < target:
         return binary_search(f, target, midpoint, right, tol)
-    elif (f(midpoint) >= target):
+    elif f(midpoint) >= target:
         return binary_search(f, target, left, midpoint, tol)
     else:
-        print("we should never get down here")    
+        print("Error - we should never get down here")
 
-def a_max(f,lst):
-    a_max = lst[0]
+
+def a_max(f, lst):
+    """Finds max"""
+    val_max = lst[0]
     for l in lst:
-        if f(l) > f(a_max):
-            a_max = l
-    return a_max
+        if f(l) > f(val_max):
+            val_max = l
+    return val_max
 
-# ternary search on the equalized criterion (loan rate, ppv, etc..)
+
 def ternary_maximize(f, left=1e-5, right=1 - 1e-5, tol=1e-5):
+    """ternary search on the equalized criterion (loan rate, ppv, etc.)"""
     m_1 = (2. / 3) * left + (1. / 3) * right
     m_2 = (1. / 3) * left + (2. / 3) * right
     if abs(m_1 - m_2) < tol:
-        return a_max(f,[m_1,m_2])
+        return a_max(f, [m_1, m_2])
     if f(m_1) < f(m_2):
         return ternary_maximize(f, m_1, right, tol)
     if f(m_1) >= f(m_2):
         return ternary_maximize(f, left, m_2, tol)
     else:
-        print("fly you fools - we should never get down here")
+        print("Error - we should never get down here")
 
-# takes in a loan rate between 0 and 1 and a pdf
-# returns a vector of the optimal loaning per score
-# that achieves the rate 
-def rate_to_loans(rate,pdf):
+
+def rate_to_loans(rate, pdf):
+    """takes in a loan rate between 0 and 1 and a pdf
+    returns a vector of the optimal loaning per score
+    that achieves the rate"""
     output = np.zeros(len(pdf))
     total = 0
     for i in range(len(pdf)):
-        if pdf[-i-1] + total > rate:
+        if pdf[-i - 1] + total > rate:
             thing = rate - total
-            output[-i-1] = thing/pdf[-i-1]
+            output[-i - 1] = thing / pdf[-i - 1]
             return output
         else:
-            output[-i-1] = 1
-            total = total+pdf[-i-1]
+            output[-i - 1] = 1
+            total = total + pdf[-i - 1]
 
     return output
 
-def loans_to_rate(loans,pdf):
-    return sum(pdf*loans)
 
-# computes the tp of a given loan rate
-def rate_to_tp(rate,pdf,perf):
-    loans = rate_to_loans(rate,pdf)
-    return loans_to_tp(loans,pdf,perf)
+def loans_to_rate(loans, pdf):
+    """get acceptance rate"""
+    return sum(pdf * loans)
 
-def loans_to_tp(loans,pdf,perf):
-    would_pay = np.sum(pdf*perf)
-    will_pay = np.sum(pdf*perf*loans)
-    return will_pay/would_pay
 
-def tp_to_loan_rate(tp,pdf,perf):
-    f = lambda t: rate_to_tp(t,pdf,perf)
-    rate =  binary_search(f , target = tp)
+def rate_to_tp(rate, pdf, perf):
+    """computes the tp of a given loan rate"""
+    loans = rate_to_loans(rate, pdf)
+    return loans_to_tp(loans, pdf, perf)
+
+
+def loans_to_tp(loans, pdf, perf):
+    """get true positive rate"""
+    would_pay = np.sum(pdf * perf)
+    will_pay = np.sum(pdf * perf * loans)
+    return will_pay / would_pay
+
+
+def tp_to_loan_rate(tp, pdf, perf):
+    """get acceptance rate from true positive rate"""
+    f = lambda t: rate_to_tp(t, pdf, perf)
+    rate = binary_search(f, target=tp)
     return rate
 
-def tp_to_loans(tp,pdf,perf):
-    rate = tp_to_loan_rate(tp,pdf,perf)
-    loans = rate_to_loans(tp_to_loan_rate(tp,pdf,perf), pdf)
+
+def tp_to_loans(tp, pdf, perf):
+    """get loans policy from true positive rate"""
+    rate = tp_to_loan_rate(tp, pdf, perf)
+    loans = rate_to_loans(rate, pdf)
     return loans
 
-def profit_from_roc():
-    return 
 
 class fairness_data:
-    # @param perf[i][j] = performance of class i, score j
-    # @param pdf[i][j] = fraction of class i w/ score j
-    # @param props[i] = proportion of popultion in class i
-    # @param break_even = loan rate yielding break_even (
-    # equivalent to loan utility)
+    """@param perf[i][j] = performance of class i, score j
+       @param pdf[i][j] = fraction of class i w/ score j
+       @param props[i] = proportion of popultion in class i
+       @param break_even = loan rate yielding break_even (equivalent to loan utility)"""
+
     def __init__(self, perf, pdf, props, break_even):
         self.perf = perf
         self.pdf = pdf
@@ -89,60 +103,68 @@ class fairness_data:
         self.num_groups = len(props)
         self.break_even = break_even
 
-    def update_break_even(break_even):
+    def update_break_even(self, break_even):
+        """update break even point"""
         self.break_even = break_even
 
-    def update_pdf(pdf):
+    def update_pdf(self, pdf):
+        """update pdf"""
         self.pdf = pdf
 
-    def loans_from_rate(self,rate):
+    def loans_from_rate(self, rate):
+        """get loans from rate"""
         loans = np.zeros(self.pdf.shape)
         for i in range(self.num_groups):
-            loans[i] = rate_to_loans(rate,self.pdf[i])
+            loans[i] = rate_to_loans(rate, self.pdf[i])
         return loans
 
-    def loans_from_tp(self,tp):
+    def loans_from_tp(self, tp):
+        """get loans from true positive rate"""
         loans = np.zeros(self.pdf.shape)
         for i in range(self.num_groups):
-            loans[i] = tp_to_loans(tp,self.pdf[i],self.perf[i])      
+            loans[i] = tp_to_loans(tp, self.pdf[i], self.perf[i])
         return loans
 
-    def compute_profit(self,break_even,loans):
-        in_groups_prof = np.sum((self.perf - break_even) * loans*self.pdf, axis = 1)
+    def compute_profit(self, break_even, loans):
+        """compute profit"""
+        in_groups_prof = np.sum((self.perf - break_even) * loans * self.pdf, axis=1)
         return np.dot(self.props, in_groups_prof)
-    
-    def get_break_even(self,break_even):
-        if break_even == None:
+
+    def get_break_even(self, break_even):
+        """get break even"""
+        if break_even is None:
             return self.break_even
         return break_even
 
-    # for testing purposes
     def graph_dem_parity(self):
-        f_prof = lambda rate: self.compute_profit(self.break_even,self.loans_from_rate(rate))
+        """for testing purposes"""
+        f_prof = lambda rate: self.compute_profit(self.break_even, self.loans_from_rate(rate))
         rates = []
         profits = []
         for t in range(10):
-            rate = .899 +  t/1000.0
+            rate = .899 + t / 1000.0
             rates.append(rate)
             print(self.loans_from_rate(rate))
             profits.append(f_prof(rate))
         print(ternary_maximize(f_prof))
-        plt.plot(rates,profits)
-        return rates,profits
+        plt.plot(rates, profits)
+        return rates, profits
 
-    def demographic_loans(self, break_even = None):
+    def demographic_loans(self, break_even=None):
+        """return loan policy under demographic parity"""
         break_even = self.get_break_even(break_even)
-        f_prof = lambda rate: self.compute_profit(break_even,self.loans_from_rate(rate))
-        target_rate = ternary_maximize(f_prof) 
+        f_prof = lambda rate: self.compute_profit(break_even, self.loans_from_rate(rate))
+        target_rate = ternary_maximize(f_prof)
         return self.loans_from_rate(target_rate)
 
-    def max_profit_loans(self,break_even = None):
+    def max_profit_loans(self, break_even=None):
+        """return loan policy under max profit"""
         break_even = self.get_break_even(break_even)
         return np.trunc(self.perf + 1 - break_even)
 
-    def equal_opp_loans(self,break_even = None):
+    def equal_opp_loans(self, break_even=None):
+        """return loan policy under equal opportunity"""
         break_even = self.get_break_even(break_even)
-        f_prof = lambda tp: self.compute_profit(break_even,self.loans_from_tp(tp))
+        f_prof = lambda tp: self.compute_profit(break_even, self.loans_from_tp(tp))
         target_tp = ternary_maximize(f_prof)
         return self.loans_from_tp(target_tp)
-   
